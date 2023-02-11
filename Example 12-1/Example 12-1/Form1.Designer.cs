684079bb4ea18bc3d628866738d7d4dc045b242b
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 18:15:16 +0330

    Add files via upload

---
+++ Example 12-1/Example 12-1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Example_12_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Location = new System.Drawing.Point(153, 231);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(116, 49);
+            this.btnRemoveAll.TabIndex = 0;
+            this.btnRemoveAll.Text = "Remove All";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 292);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Name = "Form1";
+            this.Text = "Example 12-1 (Control Remove & Random Create)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Example 12-1/Example 12-1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_12_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //--------------------------------------------------------------
+        private void ClearForm_NotRemoveAllButton()
+        {
+            this.Controls.SetChildIndex(btnRemoveAll, 0);
+            while (this.Controls.Count > 1)
+                this.Controls[1].Dispose();
+        }
+        //--------------------------------------------------------------
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+            ClearForm_NotRemoveAllButton();
+        }
+        //--------------------------------------------------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int ControlNumber = rand.Next(1, 8);// [1,8) = [1,7] = 7 ta control
+            Control newControl = new Control();
+            switch(ControlNumber)
+            {
+                case 1: 
+                    newControl = new Label();
+                    newControl.Text = "Label";
+                    break;
+                case 2:
+                    newControl = new Button();
+                    newControl.Text = "Button";
+                    break;
+                case 3:
+                    newControl = new TextBox();
+                    newControl.Text = "TextBox";
+                    break;
+                case 4:
+                    newControl = new CheckBox();
+                    newControl.Text = "CheckBox";
+                    break;
+                case 5:
+                    newControl = new RadioButton();
+                    newControl.Text = "RadioButton";
+                    break;
+                case 6:
+                    newControl = new ListBox();
+                    newControl.Text = "ListBox";
+                    break;
+                case 7:
+                    newControl = new ComboBox();
+                    newControl.Text = "ComboBox";
+                    break;
+            }
+            newControl.Location = new Point(rand.Next(this.Width), rand.Next(this.Top));
+            this.Controls.Add(newControl);
+            ///////
+            timer1.Interval = rand.Next(100, 1001);//[0.1,1] second;
+        }
+    }
+}
